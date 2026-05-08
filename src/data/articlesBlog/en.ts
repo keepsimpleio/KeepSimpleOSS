a30d38b5eeb6b2e4fed604c4134e3005b4d31fe0
MaryWylde <marykhachatryan13@gmail.com>
2026-05-08 15:05:04 +0400

    chore: new article type - AI

--- src/data/articlesBlog/en.ts
+++ src/data/articlesBlog/en.ts
@@ -1,8 +1,10 @@
 const en = {
   uxcore: 'UX Core',
+  ai: 'Artificial Intelligence & Vibecoding',
   thoughts: 'Thoughts',
   pm: 'Project Management',
   uxcoreId: 'UX Core',
+  aiId: 'AI',
   thoughtsId: 'Thoughts',
   pmId: 'Project Management',
 };
--- src/data/articlesBlog/ru.ts
+++ src/data/articlesBlog/ru.ts
@@ -1,8 +1,10 @@
 const ru = {
   uxcore: 'UX Core',
+  ai: 'Искусственный Интеллект и Вайбкодинг',
   thoughts: 'Разное',
   pm: 'Управление проектами',
   uxcoreId: 'UX Core',
+  aiId: 'AI',
   thoughtsId: 'Thoughts',
   pmId: 'Project Management',
 };
